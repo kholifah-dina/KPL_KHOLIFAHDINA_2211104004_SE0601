9fb216c7beb0ee1d728881712ad938e1acb4440b
kholifah-dina <kholifahdinaa@gmail.com>
2025-03-12 21:43:28 +0700

    TP modul 5

---
+++ 05_Generics/tpmodul5_2211104004/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+        Console.ReadLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Dina"); // Ganti dengan nama panggilan praktikan
+    }
+}
