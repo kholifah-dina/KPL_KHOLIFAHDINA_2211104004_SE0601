af8b140630d01f9e248ea1422801fef9190d1d77
kholifah-dina <kholifahdinaa@gmail.com>
2025-05-02 20:01:33 +0700

    MENGUMPULKAN FILE GUIDED MODUL 9

---
+++ GUIDED/guided_modul9/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApiProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            return Ok("Hello from Swagger API!");
+        }
+    }
+}
